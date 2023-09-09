b701c1c879f6bbfa5d4926e6c43a2cff4e427c82
Kirbro36 <120019115+Kirbro36@users.noreply.github.com>
2023-09-09 19:56:52 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const { PORT = 3000, DB_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '64fc5e221403275f16708640',
+  };
+  next();
+});
+
+app.use('/users', require('./routes/users'));
+app.use('/cards', require('./routes/cards'));
+
+app.use('*', (req, res) => {
+  res.status(404).send({ message: 'Страница не найдена.' });
+});
+
+app.listen(PORT);
