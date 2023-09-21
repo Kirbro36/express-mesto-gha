fc2e50f868c2e18cc143e886acdc153b5d23b905
Kirbro36 <120019115+Kirbro36@users.noreply.github.com>
2023-09-21 23:14:23 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const bodyParser = require('body-parser');
+const { errors } = require('celebrate');
+const errorHandler = require('./middlewares/error-handler');
+
+const { PORT = 3000, DB_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
+
+const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+app.use(limiter);
+
+app.use(helmet());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use('/', require('./routes/index'));
+
+app.use(errors());
+
+app.use(errorHandler);
+
+app.listen(PORT);
